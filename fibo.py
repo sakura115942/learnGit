86a95b5004181f65c065343616c1ddd9dc9cad14
chan <dd115942@gmail.com>
2021-12-22 19:02:45 +0800

    add fibo funcition

---
+++ fibo.py
@@ -0,0 +1,12 @@
+
+
+def fibo(n):
+    if n==0:
+        return 0
+    if n==1:
+        return 1
+    return fibo(n-1)+fibo(n-2)
+
+if __name__ == '__main__':
+    for i in range(10):
+        print(fibo(i), end=' ')
